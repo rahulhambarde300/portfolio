b25471960ad66c74ac01c0d39c6c3198f2e8931f
Rahul <rahulhambarde300@gmail.com>
2020-08-27 18:10:35 +0530

    Shifted to heroku

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/HeaderComponent';
 import Home from './components/HomeComponent';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Profile from './components/ProfileComponent';
-import Work from './components/WorkComponent';
-import Contact from './components/ContactComponent';
 
 
 function App() {
