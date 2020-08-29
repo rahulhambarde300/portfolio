b7e0ced7441bd6f1fb6415970be3ccdba6d82976
Rahul <rahulhambarde300@gmail.com>
2020-08-29 10:38:12 +0530

    Description added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './components/HeaderComponent';
 import Home from './components/HomeComponent';
-import Back from './Images/Backgrounds/backgrounds-19.jpg'
 
 
 function App() {
   return (
-      <div className="App" style={{ background : '#222629' 
-        }}>
+      <div className="App" style={{ background : '#222629'}}>
         <Header />
         <Home/>
       </div>
